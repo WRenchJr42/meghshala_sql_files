2d3c7ad2848d761291da50818267cb15b79447b6
Yashas P <130992790+WRenchJr42@users.noreply.github.com>
2025-05-01 16:57:58 +0530

    Create semesters.sql

---
+++ semesters.sql
@@ -0,0 +1,36 @@
+-- semester table
+
+CREATE TABLE public.semesters (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  number varchar,                                                         --ask
+  --name text NOT NULL UNIQUE,
+  
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  updated_at timestamp with time zone NOT NULL DEFAULT now(),
+  updated_by uuid references auth.users(id),
+  created_by uuid references auth.users(id)
+);
+
+
+-- Trigger for INSERT (for created_by)
+
+CREATE TRIGGER semesters_set_user_ids_insert
+  BEFORE INSERT ON public.semesters
+  FOR EACH ROW
+  EXECUTE FUNCTION public.set_user_ids();
+
+
+-- Trigger for UPDATE (for updated_by)
+
+CREATE TRIGGER semesters_set_user_ids_update
+  BEFORE UPDATE ON public.semesters
+  FOR EACH ROW
+  EXECUTE FUNCTION public.set_user_ids();
+
+
+-- Trigger for UPDATE (for updated_at)
+
+CREATE TRIGGER semesters_set_updated_at
+  BEFORE UPDATE ON public.semesters
+  FOR EACH ROW
+  EXECUTE FUNCTION public.update_updated_at_column();
