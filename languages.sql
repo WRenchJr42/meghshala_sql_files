bed7314c221b34856ae9f2ebed00fe8a5f313714
Yashas P <130992790+WRenchJr42@users.noreply.github.com>
2025-05-01 16:52:03 +0530

    Update languages.sql

--- languages.sql
+++ languages.sql
@@ -1,35 +1,35 @@
---Create table
+-- Create Table
 
-CREATE TABLE public.curricula (
-  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),                   -- ID 
-  title text NOT NULL UNIQUE,                                      -- Name of the curriculum
-  
-  created_at timestamp with time zone NOT NULL DEFAULT now(),
-  updated_at timestamp with time zone NOT NULL DEFAULT now(),
-  updated_by uuid references auth.users(id),
-  created_by uuid references auth.users(id)
+CREATE TABLE public.languages (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),                -- ID
+  title text NOT NULL UNIQUE,                                   -- Language Name
+
+  created_at timestamp with time zone DEFAULT now(),
+  updated_at timestamp with time zone DEFAULT now(),
+  created_by uuid REFERENCES auth.users(id),
+  updated_by uuid REFERENCES auth.users(id) 
 );
 
 
 -- Trigger for INSERT (for created_by)
 
-CREATE TRIGGER curricula_set_user_ids_insert
-  BEFORE INSERT ON public.curricula
+CREATE TRIGGER languages_set_user_ids_insert
+  BEFORE INSERT ON public.languages
   FOR EACH ROW
   EXECUTE FUNCTION public.set_user_ids();
 
 
 -- Trigger for UPDATE (for updated_by)
 
-CREATE TRIGGER curricula_set_user_ids_update
-  BEFORE UPDATE ON public.curricula
+CREATE TRIGGER languages_set_user_ids_update
+  BEFORE UPDATE ON public.languages
   FOR EACH ROW
   EXECUTE FUNCTION public.set_user_ids();
 
 
 -- Trigger for UPDATE (for updated_at)
 
-CREATE TRIGGER curricula_set_updated_at
-  BEFORE UPDATE ON public.curricula
+CREATE TRIGGER languages_set_updated_at
+  BEFORE UPDATE ON public.languages
   FOR EACH ROW
   EXECUTE FUNCTION public.update_updated_at_column();
