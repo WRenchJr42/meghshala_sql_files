5983f2d20d0d7a13fabab9316fec47f08b6bdd52
Yashas P <130992790+WRenchJr42@users.noreply.github.com>
2025-05-01 16:52:27 +0530

    Update grades.sql

--- grades.sql
+++ grades.sql
@@ -1,8 +1,8 @@
---Create table
+--grade table
 
-CREATE TABLE public.curricula (
-  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),                   -- ID 
-  title text NOT NULL UNIQUE,                                      -- Name of the curriculum
+CREATE TABLE public.grades (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),                  -- ID
+  number integer NOT NULL UNIQUE,                                 -- Grade number
   
   created_at timestamp with time zone NOT NULL DEFAULT now(),
   updated_at timestamp with time zone NOT NULL DEFAULT now(),
@@ -10,26 +10,25 @@ CREATE TABLE public.curricula (
   created_by uuid references auth.users(id)
 );
 
-
 -- Trigger for INSERT (for created_by)
 
-CREATE TRIGGER curricula_set_user_ids_insert
-  BEFORE INSERT ON public.curricula
+CREATE TRIGGER grades_set_user_ids_insert
+  BEFORE INSERT ON public.grades
   FOR EACH ROW
   EXECUTE FUNCTION public.set_user_ids();
 
 
 -- Trigger for UPDATE (for updated_by)
 
-CREATE TRIGGER curricula_set_user_ids_update
-  BEFORE UPDATE ON public.curricula
+CREATE TRIGGER grades_set_user_ids_update
+  BEFORE UPDATE ON public.grades
   FOR EACH ROW
   EXECUTE FUNCTION public.set_user_ids();
 
 
 -- Trigger for UPDATE (for updated_at)
 
-CREATE TRIGGER curricula_set_updated_at
-  BEFORE UPDATE ON public.curricula
+CREATE TRIGGER grades_set_updated_at
+  BEFORE UPDATE ON public.grades
   FOR EACH ROW
   EXECUTE FUNCTION public.update_updated_at_column();
