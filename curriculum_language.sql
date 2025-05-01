535a95780c994305c3498b5725f50569f5b4c67e
Yashas P <130992790+WRenchJr42@users.noreply.github.com>
2025-05-01 16:46:47 +0530

    Delete curriculum_language.sql

--- curriculum_language.sql
+++
@@ -1,9 +0,0 @@
-create table public.curriculum_language (
-  id uuid primary key default uuid_generate_v4(),
-  language languages,
-
-  created_at timestamp with time zone NOT NULL DEFAULT now(),
-  updated_at timestamp with time zone NOT NULL DEFAULT now(),
-  updated_by uuid references auth.users(id),
-	created_by uuid references auth.users(id) 
-);
